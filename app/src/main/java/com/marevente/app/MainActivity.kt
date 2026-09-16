52255bb880600cb0e39ec947a2a67d893f989653
tmthomasmm-star <tm.thomasmm@gmail.com>
2026-09-16 21:08:21 +0200

    Update MainActivity.kt

--- app/src/main/java/com/marevente/app/MainActivity.kt
+++ app/src/main/java/com/marevente/app/MainActivity.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
